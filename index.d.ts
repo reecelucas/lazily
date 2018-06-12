2db8da8ad2a6e6b1904501f6a3a0f1a68dedcc5b
Reece Lucas <reece.lucas@bbc.co.uk>
2018-06-12 12:38:03 +0100

    Update TypeScript declaration file to include module declaration

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,13 @@
+/**
+ * `lazily` is a UMD module that exposes a global variable when loaded
+ * outside of a module loader environment, so we must declare the global
+ * function name here.
+ */
+export as namespace lazily;
+
+// Specify that the function is the exported object from the file.
+export = lazily;
+
 interface LazilyOptions {
     selector?: string;
     loadClass?: string;
